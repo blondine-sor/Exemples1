d7942468f48ece44641a8cb0c301dd865894a824
blondine-sor <bkeumani@gmail.com>
2023-09-20 14:51:25 -0400

    init

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+$team1_data =["Canadiens",3];
+
+echo $team1_data[0];
+$team1_data =[
+    "name"=> "Canadiens",
+    "score"=> 3
+];
+$team2_data =[
+    "name"=> "Bulldogs",
+    "score"=> 5
+];
+
+
+
+
+?>
